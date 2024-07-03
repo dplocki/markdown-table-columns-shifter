55911c0c70e970bc28aaf6da7bc84b76c2e6414d
dplocki <dawid.plocki@gmail.com>
2024-07-03 19:36:16 +0200

    chore: switch jest configuration into typescript

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
+
+export default config;
