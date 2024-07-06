145acbeddbd86069691bea6f4a35c84773f3744d
dplocki <dawid.plocki@gmail.com>
2024-07-06 18:25:35 +0200

    refactor: reduce the line number for unit test for movemarkdowncolumns

--- tests/table.shift.test.ts
+++ tests/table.shift.test.ts
@@ -56,9 +56,7 @@ describe('getColumnLayout', () => {
 
 describe('moveMarkdownColumns', () => {
   it('should return empty test getting empty text', () => {
-    const result = moveMarkdownColumns([], '');
-
-    expect(result).toBe('');
+    expect(moveMarkdownColumns([], '')).toBe('');
   });
 
   it('should return what get if that is not a markdown table', () => {
