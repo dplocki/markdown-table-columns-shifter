d2873840913ce89fe3ba66668719ebbbe5f09662
dplocki <dawid.plocki@gmail.com>
2024-07-06 13:40:12 +0200

    test: add string generator function

--- tests/table.shift.test.ts
+++ tests/table.shift.test.ts
@@ -1,5 +1,15 @@
 import { moveMarkdownColumns } from "../src/table.shift";
 
+const randomString = (number, prefix = '') => {
+  while (number--) {
+    let tmp = Math.random() * 62 | 0;
+    tmp += tmp > 9 ? (tmp < 36 ? 55 : 61) : 48;
+    prefix += String.fromCharCode(tmp);
+  }
+
+  return prefix;
+};
+
 describe('moveMarkdownColumns', () => {
   it('should return empty test getting empty text', () => {
     const result = moveMarkdownColumns([], '');
@@ -8,7 +18,7 @@ describe('moveMarkdownColumns', () => {
   });
 
   it('should return what get if that is not a markdown table', () => {
-    const table = 'sddsds';
+    const table = randomString(10);
     const result = moveMarkdownColumns([], table);
 
     expect(result).toBe(table);
