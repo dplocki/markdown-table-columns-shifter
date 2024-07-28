cef8ac676feeed9fdec0b2dda65ad062a78779f3
dplocki <dawid.plocki@gmail.com>
2024-07-28 14:27:09 +0200

    chore: add output for the indent examples

--- examples/indent.ts
+++ examples/indent.ts
@@ -10,11 +10,29 @@ const originalTable = `        | Name    | Age | City       |
 
 console.log('Oryginal table:');
 console.log(originalTable);
+//         | Name    | Age | City       |
+//         |---------|-----|------------|
+//         | Alice   | 30  | New York   |
+//         | Bob     | 25  | Los Angeles|
+//         | Charlie | 35  | Chicago    |
+//         | Diana   | 28  | Houston    |
 
 console.log()
 console.log('Remove column:')
 console.log(markdownTableColumnsShift([0, 1], originalTable));
+//         | Name    |
+//         |---------|
+//         | Alice   |
+//         | Bob     |
+//         | Charlie |
+//         | Diana   |
 
 console.log()
 console.log('Remove padding:')
 console.log(markdownTableColumnsShift([1, 2, 3], originalTable));
+// | Name    | Age | City       |
+// |---------|-----|------------|
+// | Alice   | 30  | New York   |
+// | Bob     | 25  | Los Angeles|
+// | Charlie | 35  | Chicago    |
+// | Diana   | 28  | Houston    |
